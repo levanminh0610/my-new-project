9bc6233f016b84e5c5b6ef2c33e4ae8215dc7e55
Admins <admins@Adminss-MacBook-Air.local>
2019-09-19 15:48:05 +0700

    gui du lieu len

---
+++ sesion 2/baitap1.js
@@ -0,0 +1,10 @@
+
+let Chemistry=Number(prompt("hay nhap vao: "));
+let physic=Number(prompt("hay nhap vao day: "));
+let biolo=Number(prompt("hay nhap vao: "));
+medium=(Chemistry+physic+biolo)/3;
+alert("xem nhu the nao: " + medium);
+document.write("xem nhu the nao: " + medium );
+
+
+
